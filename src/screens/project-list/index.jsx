37f5961380e1d5c7d0cfab4d73a263100bb8fbd1
zxp <825077215@qq.com>
2022-03-25 17:21:50 +0800

    init the new project

---
+++ src/screens/project-list/index.jsx
@@ -0,0 +1,36 @@
+import * as qs from "qs";
+import { useEffect, useState } from "react";
+import { cleanObject, useMount, useDebounce } from "utils";
+import { List } from "./list";
+import { SearchPanel } from "./search-panel";
+const apiUrl = process.env.REACT_APP_API_URL
+
+export const ProjectListScreen = () => {
+    const [param, setParam] = useState({
+        name: '',
+        personId: ''
+    })
+    const debouncedParam = useDebounce(param, 2000)
+    const [users, setUsers] = useState([])
+
+    const [list, setList] = useState([])
+    
+    useEffect(() => {
+        fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async response => {
+            if (response.ok) {
+                setList(await response.json())
+            }
+        })
+    }, [debouncedParam])
+    useMount(() => {
+        fetch(`${apiUrl}/users`).then(async response => {
+            if (response.ok) {
+                setUsers(await response.json())
+            }
+        })
+    })
+    return <div>
+        <SearchPanel users={users} param={param} setParam={setParam} />
+        <List users={users} list={list} />
+    </div>
+}
